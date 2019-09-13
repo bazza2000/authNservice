8b55c233881a6e4f33a021c389dccc0cafb262ea
Jim Lack <jlack35@gmail.com>
2019-09-13 09:25:36 +0100

    add CORS filter

--- src/main/java/viosystems/digital/demo/DemoApplication.java
+++ src/main/java/viosystems/digital/demo/DemoApplication.java
@@ -3,11 +3,34 @@ package viosystems.digital.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import viosystems.digital.telemetry.EndpointIdentifierAspect;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class DemoApplication {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> initCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "clientHeaders"));
+        config.addAllowedMethod("*");
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
