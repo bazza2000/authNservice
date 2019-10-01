0b82876379aabffc72d180169a2b8d222201513d
Jim Lack <jlack35@gmail.com>
2019-10-01 12:30:33 +0100

    breaking change

--- src/main/java/viosystems/digital/authn/dto/UserProfileDto.java
+++ src/main/java/viosystems/digital/authn/dto/UserProfileDto.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Builder
 public class UserProfileDto {
     private String firstName;
-    private String surname;
+    private String surName;
     private String email;
     private String username;
 }
